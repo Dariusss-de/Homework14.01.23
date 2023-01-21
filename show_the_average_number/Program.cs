405fad4bc868a58ea4714e665949e4b20749df21
Dmitry <mangosovdima@mail.ru>
2023-01-21 21:14:35 +0300

    added show_the_average_number

---
+++ show_the_average_number/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+while(number>=100)
+{
+    number /= 10;
+}
+int res = number%10;
+Console.WriteLine("Число " +number);
+Console.WriteLine("Десятки " +res);
+
+/*Второй вариант
+Console.Write("Введите число: ");
+int number = new Random().Next(100,999);
+Console.WriteLine("Число: "+number);
+while(number >=100)
+{
+    number /= 10;
+}
+int res = number%10;
+Console.WriteLine("Десятки " +res);
+*/
