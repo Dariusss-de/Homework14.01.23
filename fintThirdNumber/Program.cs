93e1e93295dcaa4f41f8175819700a7dd8570b7d
Dmitry <mangosovdima@mail.ru>
2023-01-21 21:10:43 +0300

    added findThirdNumber

---
+++ fintThirdNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = -1;
+if (number >= 100)
+{
+while (number > 999)
+{
+number = number / 10;
+}
+result = number % 10;
+}
+if(result == -1) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine("Третье число: "+result);
