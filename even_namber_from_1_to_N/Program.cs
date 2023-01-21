c02d5989b69ea0654a9ca0204942fe2e9d9d2431
Dmitry <mangosovdima@mail.ru>
2023-01-21 21:09:10 +0300

    added even_number_from_1_to_N

---
+++ even_namber_from_1_to_N/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+for(int i=1;i<=N;i++)
+{
+    if(i%2 == 0) Console.WriteLine("Число "+i+" является чётным.");
+   // else Console.WriteLine("Число "+i+" является нечётным");
+}
+
+/*Второй вариант с генератором
+int N = new Random().Next(1,45);
+for(int i=1;i<=N;i++)
+{
+    if(i%2 == 0) Console.WriteLine("Число "+i+" является чётным.");
+    else Console.WriteLine("Число "+i+" является нечётным");
+}
+*/
